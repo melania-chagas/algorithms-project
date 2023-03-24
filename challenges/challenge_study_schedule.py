002a5a53d8b35d35ba5dfc393750af03e186e9fb
Melania P Chagas <melaniapchagas@gmail.com>
2023-03-24 12:04:32 -0300

    feat: implementando contador de estudantes para determinado 'target_time'

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,4 +1,4 @@
-def study_schedule(permanence_period, target_time):
+def parameter_validation(permanence_period, target_time):
     if not target_time:
         return None
 
@@ -9,6 +9,19 @@ def study_schedule(permanence_period, target_time):
             return None
 
     return 'Tudo ok!'
+
+
+def study_schedule(permanence_period, target_time):
+    if parameter_validation(permanence_period, target_time) == 'Tudo ok!':
+        number_of_students = 0
+        for period in permanence_period:
+            if period[0] == target_time:
+                number_of_students += 1
+            if target_time > period[0] and target_time <= period[1]:
+                number_of_students += 1
+
+        return number_of_students
+
 
 # permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
 # target_time = 5
