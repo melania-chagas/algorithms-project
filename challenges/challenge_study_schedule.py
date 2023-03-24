d559407f9ed85ae587b6396d13e6e852edd3b952
Melania P Chagas <melaniapchagas@gmail.com>
2023-03-24 11:13:43 -0300

    feat: implementando testes de validação para 'permanence_period' e 'target_time'

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not target_time:
+        return None
+
+    for period in permanence_period:
+        if type(period[0]) is not int or type(period[1]) is not int:
+            return None
+        if period[0] is None or period[1] is None:
+            return None
+
+    return 'Tudo ok!'
+
+# permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+# target_time = 5
+# print(study_schedule(permanence_period, target_time))
