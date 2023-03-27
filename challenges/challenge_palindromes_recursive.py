a2eaf42df582f5598edb756fe35d9c53b699adc9
Melania P Chagas <melaniapchagas@gmail.com>
2023-03-27 12:45:06 -0300

    feat: implementando função que verifica se uma palavra é palíndromo

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,10 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word or word[low_index] != word[high_index]:
+        return False
+    elif (len(word) == 1) or high_index <= low_index:
+        return True
+    else:
+        low_index += 1
+        high_index -= 1
+
+    return is_palindrome_recursive(word, low_index, high_index)
